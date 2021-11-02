5d6ad65e0da77fbb347494f2a1b31e2ae66f620b
Heng WU <wuheng@iscas.ac.cn>
2021-11-02 10:16:16 +0800

    support filter annotation

--- src/main/java/io/github/testfrk/Analyzer.java
+++ src/main/java/io/github/testfrk/Analyzer.java
@@ -19,6 +19,8 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.github.kubesys.httpfrk.utils.JavaUtil;
 
+import io.github.testfrk.utils.ValueUtil;
+
 /**
  * 
  * @author wuheng@iscas.ac.cn
@@ -26,7 +28,15 @@ import com.github.kubesys.httpfrk.utils.JavaUtil;
  */
 public class Analyzer {
 
+	protected final ValueUtil valueUtil;
+	
+
 	public Analyzer(String pkgName) throws Exception {
+		this(new ValueUtil(), pkgName);
+	}
+	
+	public Analyzer(ValueUtil valueUtil, String pkgName) throws Exception {
+		this.valueUtil = valueUtil;
 		Recorder.record(Extractor.extract(
 				Scanner.scan(pkgName)));
 	}
@@ -39,7 +49,7 @@ public class Analyzer {
 				Method ana =  Analyzer.class.getMethod(
 						"analyse" + RuleBase.urlToReqType.get(url), 
 						String.class, Method.class);
-				node.set(url, (ArrayNode) ana.invoke(null, url, RuleBase.urlToMethod.get(url)));
+				node.set(url, (ArrayNode) ana.invoke(this, url, RuleBase.urlToMethod.get(url)));
 			} catch (InvocationTargetException re) { 
 				re.printStackTrace();
 				System.out.println("stop analysing " + url);
@@ -56,7 +66,7 @@ public class Analyzer {
 	/**
 	 * 请根据项目实施要求，进行二次改造
 	 */
-	public static ArrayNode analysePOST(String url, Method m) {
+	public ArrayNode analysePOST(String url, Method m) throws Exception {
 		System.out.println("---" + url);
 		if (m.getParameterCount() == 0) {
 			return new ObjectMapper().createArrayNode();
@@ -65,7 +75,6 @@ public class Analyzer {
 		ObjectNode node = new ObjectMapper().createObjectNode();
 		for (int i = 0 ; i < m.getParameterCount(); i++) {
 			Parameter p = m.getParameters()[i];
-//			print(p.getAnnotations());
 			Type t = m.getGenericParameterTypes()[i];
 			// 每个参数都必须带Validated
 			Validated v = p.getAnnotation(Validated.class);
@@ -74,8 +83,9 @@ public class Analyzer {
 			if (JavaUtil.isPrimitive(t.getTypeName())) {
 				RequestParam rp = p.getAnnotation(RequestParam.class);
 				assertNotNull(url, p, rp, RequestParam.class);
-				String key = rp.value() == null || rp.value().length() == 0 
+				String name = rp.value() == null || rp.value().length() == 0 
 											? p.getName() : rp.value(); 
+				node.set(name, valueUtil.getPrimitiveValues(m.getName(), p));
 			} 
 			// 这些情况暂时不处理
 			else if (JavaUtil.isList(t.getTypeName()) 
@@ -87,7 +97,8 @@ public class Analyzer {
 			else {
 				RequestBody rb = p.getAnnotation(RequestBody.class);
 				assertNotNull(url, p, rb, RequestBody.class);
-				System.out.println(getClassName(t.getTypeName()));
+				node = valueUtil.getObjectValues(ValueUtil
+						.getClassName(t.getTypeName()), v.value());
 			}
 		}
 		
@@ -95,25 +106,11 @@ public class Analyzer {
 		return list;
 	}
 
-	public static String getClassName(String typeName) {
-		int stx = typeName.indexOf("<");
-		if (stx == -1) {
-			return typeName;
-		}
-		int etx = typeName.indexOf(">");
-		return typeName.substring(stx + 1, etx - 1);
-	}
 	
 	public static void assertNotNull(String url, Parameter p, Annotation a, Class<?> ac) {
 		if (a == null) {
 			throw new RuntimeException("the parameter " + p.getName() + " in " + url 
 					+ " missing annotation " + ac.getName());
-		}
-	}
-	
-	public static void print(Annotation[] as) {
-		for (Annotation a : as) {
-			System.out.println(a);
 		}
 	}
 }
--- src/main/java/io/github/testfrk/utils/ValueUtil.java
+++ src/main/java/io/github/testfrk/utils/ValueUtil.java
@@ -5,7 +5,12 @@ package io.github.testfrk.utils;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 import java.util.Random;
 
@@ -15,6 +20,11 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Pattern;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.github.kubesys.httpfrk.utils.JavaUtil;
+
 /**
  * 
  * @author wuheng@iscas.ac.cn
@@ -22,6 +32,8 @@ import javax.validation.constraints.Pattern;
  */
 public class ValueUtil {
 
+	
+	protected static String[]   cstas = new String[]{"org.hibernate.validator.constraints", "javax.validation.constraints"};
 	
 	protected static Properties props = new Properties();
 	
@@ -36,6 +48,145 @@ public class ValueUtil {
 			}
 		}
 	}
+	
+	public ArrayNode getPrimitiveValues(String cls, Parameter p) {
+		return getPrimitiveValues(cls, p.getType().getName(), p.getName(), p.getAnnotations());
+	}
+	
+	public ArrayNode getPrimitiveValues(String cls, Field f, String tag) throws Exception {
+		return getPrimitiveValues(cls, f.getType().getName(), f.getName(), usedAnnotations(f.getAnnotations(), tag));
+	}
+	
+	public ArrayNode getPrimitiveValues(String cls, String type, String name, Annotation[] as) {
+		ArrayNode list = new ObjectMapper().createArrayNode();
+		if (type.equals(String.class.getName())) {
+			Annotation[] va = valuesAnnotations(as);
+			if(va.length == 0) {
+				String trueKey = cls + ".true." + name;
+				String trueVal = props.getProperty(trueKey);
+				if (trueVal != null) {
+					list.add(trueVal);
+				}
+				String falseKey = cls + ".true." + name;
+				String fasleVal = props.getProperty(falseKey);
+				if (fasleVal != null) {
+					list.add(fasleVal);
+				}
+			}
+			System.out.println("---" + name);
+			print(va);
+		}
+		return list;
+	}
+	
+	public ObjectNode getObjectValues(String clsName, Class<?>[] tags) throws Exception {
+		
+		String tag = tags.length == 0 ? null : tags[0].getName();
+		
+		ObjectNode node = new ObjectMapper().createObjectNode();
+		Class<?> cls = Class.forName(clsName);
+		for (Field f : cls.getDeclaredFields()) {
+			if (JavaUtil.isPrimitive(f.getGenericType().getTypeName())
+					|| JavaUtil.isStringList(f.getGenericType().getTypeName())) {
+				if (validParameter(f.getAnnotations(), tag)) {
+					node.set(f.getName(), getPrimitiveValues(cls.getSimpleName(), f, tag));
+				}
+			} 
+			// 这些情况暂时不处理
+			else if (JavaUtil.isList(f.getGenericType().getTypeName()) 
+					|| JavaUtil.isSet(f.getGenericType().getTypeName())
+					|| JavaUtil.isMap(f.getGenericType().getTypeName())) {
+				System.out.println("ignore, -" + f.getGenericType().getTypeName() + " " + f.getName());
+//				throw new RuntimeException("Unsupport parameters types: list, Set and Map");
+			} 
+			// 如果是Java对象，必须包含RequestBody标签
+			else {
+				node.set(f.getName(), getObjectValues(getClassName(
+						f.getGenericType().getTypeName()), tags));
+			}
+		}
+		return node;
+	}
+	
+	public static Annotation[] usedAnnotations(Annotation[] as, String tag) throws Exception {
+		
+		List<Annotation> list = new ArrayList<>();
+		for (Annotation a: valuesAnnotations(as)) {
+			Method m = a.annotationType().getDeclaredMethod("groups");
+			Class<?>[] vs =  (Class<?>[]) m.invoke(a);
+			
+			// from config case
+			if (as.length == 0 && tag == null) {
+				list.add(a);
+			}
+			
+			for (Class<?> v : vs) {
+				if (v.getTypeName().equals(tag)) {
+					list.add(a);
+				}
+			}
+		}
+		return list.toArray(new Annotation[] {});
+		
+	}
+	
+	public static boolean validParameter(Annotation[] as, String tag) throws Exception {
+	
+		for (Annotation a : valuesAnnotations(as)) {
+			Method m = a.annotationType().getDeclaredMethod("groups");
+			Class<?>[] vs =  (Class<?>[]) m.invoke(a);
+			
+			// from config case
+			if (vs.length == 0 && tag == null) {
+				return true;
+			}
+			
+			for (Class<?> v : vs) {
+				if (v.getTypeName().equals(tag)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	public static String getClassName(String typeName) {
+		int stx = typeName.indexOf("<");
+		if (stx == -1) {
+			return typeName;
+		}
+		int etx = typeName.indexOf(">");
+		return typeName.substring(stx + 1, etx);
+	}
+	
+	
+	public static Annotation[] valuesAnnotations(Annotation[] as) {
+		if (as == null) {
+			return new Annotation[] {};
+		}
+		
+		List<Annotation> list = new ArrayList<>();
+		for (Annotation a: as) {
+			for (String c : cstas) {
+				if (a.annotationType().getTypeName().contains(c)) {
+					list.add(a);
+				}
+			}
+		}
+		return list.toArray(new Annotation[] {});
+	}
+	
+	public static void print(Annotation[] as) {
+		for (Annotation a : as) {
+			System.out.println(a);
+		}
+	}
+	
+	/*******************************************************
+	 * 
+	 * 
+	 * 
+	 ********************************************************/
 	
 	static char[] codes = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
 			'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c',
